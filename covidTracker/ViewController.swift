50702f4114a49b663d4a5047d20a94c7d688b23e
Pradeep Kolli <pradeepkolli3@yahoo.in>
2020-03-24 22:55:56 -0500

    Initial Commit

---
+++ covidTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covidTracker
+//
+//  Created by pradeep Kolli on 3/24/20.
+//  Copyright © 2020 pradeep Kolli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
